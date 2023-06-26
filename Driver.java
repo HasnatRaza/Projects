ab6db0680c3ba825d4bc4687aea3d7c959b4c755
HasnatRaza <123123121+HasnatRaza@users.noreply.github.com>
2023-06-26 15:45:19 -0500

    Project 4

--- Driver.java
+++ Driver.java
@@ -1,28 +1,182 @@
+import org.openstreetmap.gui.jmapviewer.*;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
+import org.openstreetmap.gui.jmapviewer.interfaces.MapPolygon;
+import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
+import java.awt.geom.AffineTransform;
+
+
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class Driver {
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		
-		// read the data
-		TripPoint.readFile("triplog.csv");
-		
-		// find data for heuristic 1
-		System.out.println("Data for heuristic 1:");
-		System.out.println("Number of Stops: " + TripPoint.h1StopDetection());
-		System.out.println("Moving Time (hr): " + TripPoint.movingTime());
-		System.out.println("Stopped Time (hr): " + TripPoint.stoppedTime());
-		System.out.println("Average Moving Speed (km/hr): " + TripPoint.avgMovingSpeed());
-		System.out.println();
-		
-		// find data for heuristic 2
-		System.out.println("Data for heuristic 2:");
-		System.out.println("Number of Stops: " + TripPoint.h2StopDetection());
-		System.out.println("Moving Time (hr): " + TripPoint.movingTime());
-		System.out.println("Stopped Time (hr): " + TripPoint.stoppedTime());
-		System.out.println("Average Moving Speed (km/hr): " + TripPoint.avgMovingSpeed());
-	}
-	
+
+    public static int animationSec;
+    public static ArrayList<TripPoint> trip;
+    public static BufferedImage arrow;
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        TripPoint.readFile("triplog.csv");
+        TripPoint.h2StopDetection();
+        arrow = ImageIO.read(new File("arrow.png"));
+
+        // set up frame
+        JFrame frame = new JFrame("Map Viewer");
+        frame.setLayout(new BorderLayout());
+
+        // set up top panel for input selections
+        JPanel topPanel = new JPanel();
+        frame.add(topPanel, BorderLayout.NORTH);
+        // play button
+        JButton play = new JButton("Play");
+        // checkbox to enable/disable stops
+        JCheckBox includeStops = new JCheckBox("Include Stops");
+        // dropbox to pick animation time
+        String[] timeList = {"Animation Time", "15", "30", "60", "90"};
+        JComboBox<String> animationTime = new JComboBox<String>(timeList);
+        animationSec = 0;
+        // add all to top panel
+        topPanel.add(animationTime);
+        topPanel.add(includeStops);
+        topPanel.add(play);
+
+        // set up mapViewer
+        JMapViewer mapViewer = new JMapViewer();
+        frame.add(mapViewer);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mapViewer.setTileSource(new OsmTileSource.TransportMap());
+
+        // add listeners
+        play.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+                    @Override
+                    protected Void doInBackground() throws Exception {
+                        mapViewer.removeAllMapMarkers(); // remove all markers from the map
+                        mapViewer.removeAllMapPolygons();
+                        if (includeStops.isSelected()) {
+                            trip = TripPoint.getTrip();
+                        } else {
+                            trip = TripPoint.getMovingTrip();
+                        }
+                        plotTrip(animationSec, trip, mapViewer);
+                        return null;
+                    }
+                };
+                worker.execute();
+            }
+        });
+        animationTime.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    Object selectedItem = animationTime.getSelectedItem();
+                    if (selectedItem instanceof String) {
+                        String selectedString = (String) selectedItem;
+                        if (!selectedString.equals("Animation Time")) {
+                            animationSec = Integer.parseInt(selectedString);
+                            System.out.println("Updated to " + animationSec);
+                        }
+                    }
+                }
+            }
+        });
+
+        // set the map center and zoom level
+        mapViewer.setDisplayPosition(new Coordinate(34.82, -107.99), 6);
+    }
+
+    // plot the given trip ArrayList with animation time in seconds
+    public static void plotTrip(int seconds, ArrayList<TripPoint> trip, JMapViewer map) throws IOException {
+        long delayTime = (seconds * 1000) / trip.size();
+        
+        Coordinate c1;
+        Coordinate c2 = null;
+        MapMarker marker;
+        MapMarker prevMarker = null;
+        MapPolygonImpl line;
+        
+        for (int i = 0; i < trip.size(); ++i) {
+            c1 = new Coordinate(trip.get(i).getLat(), trip.get(i).getLon());
+            
+            if (i != 0) {
+                c2 = new Coordinate(trip.get(i-1).getLat(), trip.get(i-1).getLon());
+                double lat1 = Math.toRadians(c2.getLat());
+                double lon1 = Math.toRadians(c2.getLon());
+                double lat2 = Math.toRadians(c1.getLat());
+                double lon2 = Math.toRadians(c1.getLon());
+
+                double dLon = lon2 - lon1;
+                double y = Math.sin(dLon) * Math.cos(lat2);
+                double x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon);
+
+                double bearing = Math.atan2(y, x);
+                double angle = (Math.toDegrees(bearing) + 360) % 360;
+                BufferedImage rotatedArrow = rotateImageByDegrees(arrow, angle);
+                marker = new IconMarker(c1, rotatedArrow);
+            } else {
+                marker = new IconMarker(c1, arrow);
+            }
+            
+            map.addMapMarker(marker);
+            if (c2 != null) {
+                line = new MapPolygonImpl(c1, c2, c2);
+                line.setColor(Color.RED);
+                line.setStroke(new BasicStroke(3));
+                map.addMapPolygon(line);
+                map.removeMapMarker(prevMarker);
+            }
+            try {
+                Thread.sleep(delayTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            prevMarker = marker;
+        }
+    }
+
+
+
+
+
+
+    // Calculate angle between two coordinates
+    private static double calculateAngle(Coordinate c1, Coordinate c2) {
+        double angle = Math.atan2(c2.getLat() - c1.getLat(), c2.getLon() - c1.getLon());
+        return Math.toDegrees(angle);
+    }
+
+    // Rotate an image by the specified angle (in degrees)
+    public static BufferedImage rotateImageByDegrees(BufferedImage img, double angle) {
+        double radians = Math.toRadians(angle);
+        double sin = Math.abs(Math.sin(radians));
+        double cos = Math.abs(Math.cos(radians));
+        int w = img.getWidth();
+        int h = img.getHeight();
+        int newWidth = (int) Math.floor(w * cos + h * sin);
+        int newHeight = (int) Math.floor(h * cos + w * sin);
+
+        BufferedImage rotated = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = rotated.createGraphics();
+        AffineTransform at = new AffineTransform();
+        at.translate((newWidth - w) / 2, (newHeight - h) / 2);
+        at.rotate(radians, w / 2, h / 2);
+        g2d.setTransform(at);
+        g2d.drawImage(img, 0, 0, null);
+        g2d.dispose();
+
+        return rotated;
+    }
+
+
 
 }
--- README.md
+++ README.md
@@ -1,76 +1,23 @@
-# Project 3: Stop Detection
+# Exam 3: One Direction - One Way or Another
+In Exam 3, you will be extending Project 5's map GUI for the triplog dataset which we have been working with over the course of the semester.
 
-In Project 3, we will continue working with our triplog dataset. This dataset contains latitude and longitude data that was gathered over a 3 day trip. Last project, we created some useful methods to help us find aggregate data. This project, we want to further refine our data by removing excess points that are grouped together, which we will call "stops". You will create a new ArrayList which will not include the points that are stops, so that we can get a more accurate sense of the trip data, such as total time and average speed. 
+## Required New Functionality
+First, replace the raccoon head leading the path, with the given `arrow.png` which looks like this:
 
-In case you were unable to complete project 2, a starting template for the TripPoint class is included in the repo. You don't have to use it if you finished your own. You should document the methods in the template if you decide to use it. 
+<img src=./arrow.png>
 
-## Defining Stops
+Then, modify your (or the supplied Project 5 solution) code to make the arrow rotate according to the direction of travel between every two consecutive coordinates in the trip. 
 
-We need to define some heuristics for determining when a stop occurs. You will be implementing stop detection for the following two heuristics: 
+To calculate the angle between two GPS coordinates, you need to first calculate the difference between the longitude and latitude values of the two points. You can use the following formula to calculate the angle:
 
-1) The first heuristic for detecting stop zones is to check if a point is within a certain distance from the previous point. We will call this distance the "displacement threshold". If a point falls within the displacement threshold of the previous point, it should be considered a stop.
+$angle = arctan((y2 - y1) / (x2 - x1))$
 
-2) The second heuristic for detecting stop zones is to check for groups of three or more points that are all within a certain distance of each other. We will call this distance the "stop radius". If there are three or more points within the stop radius of each other, we consider them to be part of the same stop zone. This way the points do not have to be directly consecutive. For example, a point could be outside the stop radius of the point directly before it, but inside the radius of another point in the same stop zone, so that point is still considered a stop. 
+Where $x1$ and $y1$ are the longitude and latitude values of the first point, and $x2$ and $y2$ are the longitude and latitude values of the second point. The arctan function returns the angle in radians, so you may need to convert it to degrees. Then rotate the arrow by that many degrees. Repeat the process for every consecutive coordinates in `triplog.csv`, keeping in sync with the path animation.
 
-Note that "within" the distance here means it is inclusive (less than or equal to). 
+Here is how the animation should look like:
 
-## TripPoint UML
-
-<img src=./resources/UML2.PNG width=50% height=50%>
-
-Feel free to add your own helper methods as needed. 
-
-## Old Methods (These are still the same)
-
-### Constructors
-
-`TripPoint(int time, double lat, double lon)`: Initialize the class fields `time`, `lat`, and `lon`.
-
-### Getters
-
-`getTime()`: Should return `time`.
-
-`getLat()`: Should return `lat`.
-
-`getLon()`: Should return `lon`.
-
-`getTrip()`: Should return a copy of the `trip` ArrayList
-
-### Other Methods
-
-`readFile(String filename)`: Read in the data from [triplog.csv](./triplog.csv) to the `trip` ArrayList. The idea is to initialize each line of data (Time,Latitude,Longitude) as a TripPoint object. You can then fill the `trip` ArrayList with those TripPoint objects. 
-
-`totalTime()`: Should return the total time of the trip in hours. Remember the Time column in the data is in minutes. 
-
-`haversineDistance(TripPoint a, TripPoint b)`: Should compute and return the Haversine distance between two points in kilometers. 
-
-`totalDistance()`: Should compute and return the total distance of the trip in kilometers. Meaning the total distance between every point in the `trip` ArrayList. 
-
-`avgSpeed(TripPoint a, TripPoint b)`: Should return the average speed between two points in kilometers per hour. This method should work no matter which order the points are given in. 
-
-## New Methods (You should implement these for project 3)
-
-`h1StopDetection()`: Should find and return the number of stops in the `trip` ArrayList. Use the first heuristic with a displacement threshold of 0.6 km. The method should also initialize and fill the `movingTrip` ArrayList with only the points that are moving (all the TripPoints minus the stops). Don't actually remove directly from `trip`, only `movingTrip`. This way we keep both the full trip and the moving trip.
-
-`h2StopDetection()`: Should find and return the number of stops in the `trip` ArrayList. Use the second heuristic with a stop radius of 0.5 km. The method should also initialize and fill the `movingTrip` ArrayList with only the points that are moving (all the TripPoints minus the stops). Don't actually remove directly from `trip`, only `movingTrip`. This way we keep both the full trip and the moving trip.
-
-`movingTime()`: Should find and return the amount of time spent moving in hours. 
-
-`stoppedTime()`: Should find and return the amount of time spent stopped in hours. 
-
-`avgMovingSpeed()`: Should find and return the average speed over the course of the trip only considering the moving points. Units should be km/hour. 
-
-`getMovingTrip()`: Should return a copy of the `movingTrip` ArrayList.
+![Example animation](./animation.gif)
 
 ## Grading
 
-Plagiarism will not be tolerated under any circumstances. Participating students will be penalized depending on the degree of plagiarism. It includes "No-code" sharing among the students. It can lead to academic misconduct reporting to the authority if identical code is found among students. 
-
-You will be graded on: 
-* Zybooks submission: 80 points
-* Github commits (10 commits): 10 points
-* Javadocs: 10 points
-
-Note that the data file used in Zybooks is different from the one in GitHub so your solution should be a general solution.
-
-Submit your project before the due date/time. **No late submissions allowed.**
+Submit (commit and push) your exam before the due date/time. **No late submissions allowed.**
